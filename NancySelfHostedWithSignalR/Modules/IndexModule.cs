2bd93c9f97d871d587bb6a4554fd87d7f54095a3
MrDesjardins <mrdesjardins@gmail.com>
2014-09-30 20:49:59 -0700

    Initial Commit

---
+++ NancySelfHostedWithSignalR/Modules/IndexModule.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using Nancy;
+
+namespace NancySelfHostedWithSignalR.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+
+            Get[@"/{uri*}"] = parameters =>
+            {
+                var regex = new Regex(@"\.(png|gif|jpg|jpeg)$");
+                if (regex.IsMatch(parameters.uri))
+                {
+                    return View["index"];
+                }
+                else
+                {
+                    return null;
+                }
+            };
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
